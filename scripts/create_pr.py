96cea9b0692d03b839aa7e820c3064d77630c55e
Anran <mar@tradeshift.com>
2018-03-12 17:00:34 +0800

    update create_pr script to reuse 2FA code

--- scripts/create_pr.py
+++ scripts/create_pr.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+from getpass import getuser, getpass
+
 import github3
 
 try:
@@ -18,44 +22,54 @@ repos = [
     "p2p-apps",
 ]
 
-token = token_id = username = password = ''
+# use username password, two phase authentication code is required
+username = ''
+password = ''
+branch = ''
 
-
+code = ''
 def my_two_factor_function():
-    code = ''
+    global code
     while not code:
         code = prompt('Enter 2FA code: ')
     return code
 
+while not username:
+    username = prompt('Enter username: ')
+
+while not password:
+    password = getpass('Enter password: ')
+
+gh = github3.login(username, password, two_factor_callback=my_two_factor_function)
+
+
+# use token (not working cause https://github.com/sigmavirus24/github3.py/issues/602)
+# token = token_id = ''
 
 # with open('token', 'r') as fd:
 #     token = fd.readline().strip()  # Can't hurt to be paranoid
 #     token_id = fd.readline().strip()
 
-with open('user', 'r') as fd:
-    username = fd.readline().strip()  # Can't hurt to be paranoid
-    password = fd.readline().strip()
-
-gh = github3.login(username, password, two_factor_callback=my_two_factor_function)
-
-
+# gh = github3.login(token=token)
 # auth = gh.authorization(token_id)
 # auth.update(add_scopes=['repo:status', 'gist'], rm_scopes=['user'])
 
+
+while not branch:
+    branch = prompt('Enter rebase branch: ')
 
 def create_pull_request(repo_name):
     print '[%s]creating pull request' % repo_name
     try:
         repository = gh.repository('TradeshiftCN', repo_name)
         repository.create_pull(
-            title='sync 12 25',
-            base='dev',
-            head='TradeshiftCN:sync_12_25',
-            body='@Chris-Xie \
-                 @yhl10000 ',
+            title=branch,
+            base='dev-stable',
+            head='TradeshiftCN:' + branch,
+            body='@Chris-Xie ',
         )
     except Exception, e:
-        print '[%s]create pull request failed!' % repo_name
+        print '[%s]create pull request failed! Please check if the branch is exsits or the PR is created.' % repo_name
         print '     ' + str(e)
     else:
         print '[%s]create pull request successful!' % repo_name
---
+++ scripts/gen_token.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from getpass import getuser, getpass
+
+from github3 import authorize
+
+try:
+    # Python 2
+    prompt = raw_input
+except NameError:
+    # Python 3
+    prompt = input
+
+
+def my_two_factor_callback():
+    auth_code = raw_input('Please enter your 2FA code: ')
+    return auth_code
+
+
+user = prompt('Enter username: ')
+password = ''
+
+while not password:
+    password = getpass('Password for {0}: '.format(user))
+
+note = 'tradeshift-rebase-toolkit'
+note_url = 'https://cn-sandbox.tradeshift.com'
+scopes = ['user', 'repo']
+
+auth = authorize(user, password, scopes, note, note_url, two_factor_callback=my_two_factor_callback)
+
+with open('token', 'w') as fd:
+    fd.write(auth.token + '\n')
+    fd.write(str(auth.id))
