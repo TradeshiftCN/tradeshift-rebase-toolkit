860dc548a3f703e5846c666e0e6ea170a588e2b4
Yao Hu <huy@tradeshift.com>
2019-07-02 13:34:18 +0800

    Allow file PR if auto merge is successful

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -3,11 +3,17 @@ import os
 import sys
 
 import yaml
+from git import GitCommandError
+from github import Github, GithubException
+from urllib3 import Retry
 
 import config
 from utils import GitHubRepo
 
 LOGGER = logging.getLogger('pipeline')
+
+g = Github(config.GITHUB_TOKEN, retry=Retry(total=5, status_forcelist=[502]))
+tscn = g.get_organization('TradeshiftCN')
 
 
 def parse_puppet_for_heracle(workdir, puppet_repo_config, baseline_tag):
@@ -33,24 +39,40 @@ def parse_puppet_for_heracle(workdir, puppet_repo_config, baseline_tag):
 
 
 def clone_and_merge_versioned(workdir, repo_configs, version_dict):
-    for config_name, repo_configs in repo_configs.items():
+    for config_name, repo_config in repo_configs.items():
         ghrepo = GitHubRepo(workdir,
                             config_name,
-                            repo_configs['origin']['org'],
-                            repo_configs['origin']['repo_name'])
-        ghrepo.fetch(dest='origin', branch_name=repo_configs['origin']['branch'])
+                            repo_config['origin']['org'],
+                            repo_config['origin']['repo_name'])
+        ghrepo.fetch(dest='origin', branch_name=repo_config['origin']['branch'])
         if not ghrepo.is_dirty():
             ghrepo.add_remote(
-                remote_url=(f'git@github.com:{repo_configs["upstream"]["org"]}/'
-                            f'{repo_configs["upstream"]["repo_name"]}.git'),
+                remote_url=(f'git@github.com:{repo_config["upstream"]["org"]}/'
+                            f'{repo_config["upstream"]["repo_name"]}.git'),
                 remote_name='upstream')
-            hiera_version_key = f"tradeshift::components::{repo_configs['hiera-name']}::version"
+            hiera_version_key = f"tradeshift::components::{repo_config['hiera-name']}::version"
             if hiera_version_key in version_dict:
                 commit_or_tag = version_dict[hiera_version_key]
 
                 ghrepo.fetch(dest='upstream')
                 ghrepo.checkout_new_branch(commit_or_tag, config.REBASE_BRANCH_NAME)
-                ghrepo.merge_without_commit(f"origin/{repo_configs['origin']['branch']}")
+                try:
+                    ghrepo.merge_and_push_to(f"origin/{repo_config['origin']['branch']}", config.REBASE_BRANCH_NAME)
+                    ts_repo = tscn.get_repo(repo_config['origin']['repo_name'])
+                    ts_repo.create_pull(title=config.REBASE_BRANCH_NAME,
+                                        body='',
+                                        base=repo_config['origin']['branch'],
+                                        head=config.REBASE_BRANCH_NAME)
+                except GitCommandError as gce:
+                    LOGGER.warning('Merge failed, you need to look into it.', gce)
+                except GithubException as ge:
+                    if 'No commits between' in ge.data['errors'][0]['message']:
+                        LOGGER.info(
+                            f"No change between {repo_config['origin']['branch']} and config.REBASE_BRANCH_NAME,"
+                            f"No PR is created on {repo_config['origin']['repo_name']}.")
+                    else:
+                        LOGGER.error(f"Creating PR on {repo_config['origin']['repo_name']} failed.", ge)
+
             else:
                 LOGGER.error(f'version.yaml has no version of {config_name}')
         else:
@@ -71,8 +93,22 @@ def clone_and_merge_timed(workdir, repo_configs, lastest_before):
                 ghrepo.get_latest_merge_commit_before(f'upstream/{repo_config["upstream"]["branch"]}', lastest_before)
             if latest_commit_before_hiera is not None:
                 ghrepo.checkout_new_branch(latest_commit_before_hiera.hexsha, config.REBASE_BRANCH_NAME)
-                # target_branch = ghrepo.checkout(repo_config['origin']['branch'])
-                ghrepo.merge_without_commit(f"origin/{repo_config['origin']['branch']}")
+                try:
+                    ghrepo.merge_and_push_to(f"origin/{repo_config['origin']['branch']}", config.REBASE_BRANCH_NAME)
+                    ts_repo = tscn.get_repo(repo_config['origin']['repo_name'])
+                    ts_repo.create_pull(title=config.REBASE_BRANCH_NAME,
+                                        body='',
+                                        base=repo_config['origin']['branch'],
+                                        head=config.REBASE_BRANCH_NAME)
+                except GitCommandError as gce:
+                    LOGGER.warning('Merge failed, you need to look into it.', gce)
+                except GithubException as ge:
+                    if 'No commits between' in ge.data['errors'][0]['message']:
+                        LOGGER.info(
+                            f"No change between {repo_config['origin']['branch']} and config.REBASE_BRANCH_NAME,"
+                            f"No PR is created on {repo_config['origin']['repo_name']}.")
+                    else:
+                        LOGGER.error(f"Creating PR on {repo_config['origin']['repo_name']} failed.", ge)
             else:
                 LOGGER.error(f'The latest commit before baseline from {config_name} is not found.')
         else:
